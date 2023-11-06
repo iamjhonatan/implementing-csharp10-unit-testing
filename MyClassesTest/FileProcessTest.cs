43f371e8f44222cb7da4689e032ffe4d09a9a306
iamjhonatan <jhonatanmedeiros7@gmail.com>
2023-11-06 20:23:30 -0300

    refactor: creating a TestBase class and remove duplicate code

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -3,16 +3,14 @@ using MyClasses;
 namespace MyClassesTest;
 
 [TestClass]
-public class FileProcessTest
+public class FileProcessTest : TestBase
 {
-    public TestContext? TestContext { get; set; }
-
     [TestMethod]
     public void FileNameDoesExist()
     {
         // Arrange
         FileProcess fileProcess = new();
-        string fileName = TestContext?.Properties?["GoodFileName"]?.ToString() ?? TestConstants.GOOD_FILE_NAME;
+        string fileName = GetTestSetting<string>("GoodFileName", TestConstants.GOOD_FILE_NAME);
         bool fromCall;
 
         // Add Messages to Test Output
@@ -39,7 +37,7 @@ public class FileProcessTest
     {
         // Arrange
         FileProcess fileProcess = new();
-        string fileName = TestContext?.Properties?["BadFileName"]?.ToString() ?? TestConstants.BAD_FILE_NAME;
+        string fileName = GetTestSetting<string>("BadFileName", TestConstants.BAD_FILE_NAME);
         bool fromCall;
 
         // Add Messages to Test Output
@@ -67,13 +65,13 @@ public class FileProcessTest
             fileProcess = new();
 
             // Add Messages to Test Output
-            OutputMessage = TestContext?.Properties?["EmptyFileMsg"]?.ToString() ?? TestConstants.EMPTY_FILE_MSG;
+            OutputMessage = GetTestSetting<string>("EmptyFileMsg", TestConstants.EMPTY_FILE_MSG);
             TestContext?.WriteLine(TestConstants.EMPTY_FILE_MSG);
 
             fromCall = fileProcess.FileExists(fileName);
 
             // Assert: Fail as we should not get here
-            OutputMessage = TestContext?.Properties?["EmptyFileFailMsg"]?.ToString() ?? TestConstants.EMPTY_FILE_FAIL_MSG;
+            OutputMessage = GetTestSetting<string>("EmptyFileFailMsg", TestConstants.EMPTY_FILE_FAIL_MSG);
             Assert.Fail(OutputMessage);
         }
         catch (ArgumentNullException) 
@@ -94,14 +92,14 @@ public class FileProcessTest
         string OutputMessage;
 
         // Add Messages to Test Output
-        OutputMessage = TestContext?.Properties?["EmptyFileMsg"]?.ToString() ?? TestConstants.EMPTY_FILE_MSG;
+        OutputMessage = GetTestSetting<string>("EmptyFileMsg", TestConstants.EMPTY_FILE_MSG);
         TestContext?.WriteLine(OutputMessage);
 
         // Act
         fromCall = fileProcess.FileExists(fileName);
 
         // Assert: Fail as we should not get here
-        OutputMessage = TestContext?.Properties?["EmptyFileFailMsg"]?.ToString() ?? TestConstants.EMPTY_FILE_FAIL_MSG;
+        OutputMessage = GetTestSetting<string>("EmptyFileFailMsg", TestConstants.EMPTY_FILE_FAIL_MSG);
         Assert.Fail(OutputMessage);
     }
 }
---
+++ MyClassesTest/TestBase.cs
@@ -0,0 +1,24 @@
+﻿namespace MyClassesTest;
+
+public class TestBase
+{
+    public TestContext? TestContext { get; set; }
+
+    protected T GetTestSetting<T>(string name, T defaultValue)
+    {
+        T ret = defaultValue;
+
+		try
+		{
+			var temp = TestContext?.Properties[name];
+			if (temp is not null)
+				ret = (T)Convert.ChangeType(temp, typeof(T));
+		}
+		catch (Exception)
+		{
+			// Ignore exception, return the defaultValue
+		}
+
+		return ret;
+    }
+}
