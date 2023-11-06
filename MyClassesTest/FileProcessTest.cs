d2ee0d5fa4b683311e2b000d70a13399de8d92a1
iamjhonatan <jhonatanmedeiros7@gmail.com>
2023-11-06 15:51:25 -0300

    test: create first 2 tests

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -8,13 +8,31 @@ public class FileProcessTest
     [TestMethod]
     public void FileNameDoesExist()
     {
-        Assert.Inconclusive();
+        // Arrange
+        FileProcess fileProcess = new();
+        string fileName = @"C:\Windows\Regedit.exe";
+        bool fromCall;
+
+        // Act
+        fromCall = fileProcess.FileExists(fileName);
+
+        // Assert
+        Assert.IsTrue(fromCall);
     }
 
     [TestMethod]
     public void FileNameDoesNotExist()
     {
-        Assert.Inconclusive();
+        // Arrange
+        FileProcess fileProcess = new();
+        string fileName = @"C:\DoesNotExist.txt";
+        bool fromCall;
+
+        // Act
+        fromCall = fileProcess.FileExists(fileName);
+
+        // Assert
+        Assert.IsFalse(fromCall);
     }
 
     [TestMethod]
