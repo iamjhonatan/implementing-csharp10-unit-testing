2ed849882ca3858e7375387fa7232141576d1741
iamjhonatan <jhonatanmedeiros7@gmail.com>
2023-11-07 08:56:52 -0300

    test: using the Class Initialize and Cleanup Attributes

---
+++ MyClassesTest/AssemblyInitCleanup.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MyClassesTest;
+
+[TestClass]
+public class AssemblyInitCleanup
+{
+    [AssemblyInitialize()]
+    public static void AssemblyInitialize(TestContext testContext)
+    {
+        // This code runs once before all tests in this assembly
+        testContext.WriteLine("In MyClassesTest.AssemblyInitCleanup.Assembly");
+    }
+
+    [AssemblyCleanup()]
+    public static void AssemblyCleanup()
+    {
+        // This code runs once after all tests have run in this assembly
+        // NOTE: TestContext is not available in here
+    }
+}
